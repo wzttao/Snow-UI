bd2c49052cba5faefff7b3e52185ce9f9c4a3c1d
wzt <1052987294@qq.com>
2022-04-14 03:05:06 +0800

    Home页面的css

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,7 +6,7 @@
       <h2>一个厉害的 UI 框架</h2>
       <p class="actions">
         <a href="">github</a>
-        <a href="">开始</a>
+        <router-link to="/doc">开始</router-link>
       </p>
     </div>
   </div>
@@ -21,6 +21,25 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
-
+.banner {
+  padding: 100px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background: lightgreen;
+  > .actions{
+    padding: 8px 0;
+    a {
+      margin:0 8px;
+      background: #fff;
+      display: inline-block;
+      $h: 28px;
+      height: $h;
+      line-height: $h;
+      border-radius: $h/2;
+      padding: 0 8px;
+    }
+  }
+}
 </style>
